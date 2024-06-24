5cbf56cea2a0f9b4e19a50a22f3f987b0050cd67
Mark <engineermarkpro@gmail.com>
2024-06-24 10:28:41 +0200

    forgot to reenable milestones updater

--- Jobs/JobUsers.js
+++ Jobs/JobUsers.js
@@ -34,50 +34,50 @@ async function UpdateUsers() {
     });
 
     // //check if any stat went over a threshold for achievement
-    // for await (const user of remote_users) {
-    //     const local_user = local_users.find(x => x.user_id === user.user_id);
-    //     if (!local_user) continue;
+    for await (const user of remote_users) {
+        const local_user = local_users.find(x => x.user_id === user.user_id);
+        if (!local_user) continue;
 
-    //     for await (const achievement of ACHIEVEMENT_INTERVALS) {
-    //         let old_stat = 0;
-    //         let new_stat = 0;
-    //         for await (const stat of achievement.stats) {
-    //             old_stat += parseInt(local_user[stat]);
-    //             new_stat += parseInt(user[stat]);
-    //         }
+        for await (const achievement of ACHIEVEMENT_INTERVALS) {
+            let old_stat = 0;
+            let new_stat = 0;
+            for await (const stat of achievement.stats) {
+                old_stat += parseInt(local_user[stat]);
+                new_stat += parseInt(user[stat]);
+            }
 
-    //         if (old_stat === -1 || new_stat === -1) continue;
+            if (old_stat === -1 || new_stat === -1) continue;
 
-    //         let interval = achievement.interval;
+            let interval = achievement.interval;
 
-    //         if (achievement.intervalAlternative) {
-    //             for await (const alt of achievement.intervalAlternative) {
-    //                 if (alt.dir === '<' && new_stat < alt.check && interval > alt.interval) {
-    //                     interval = alt.interval;
-    //                 } else if (alt.dir === '>' && new_stat > alt.check && interval < alt.interval) {
-    //                     interval = alt.interval;
-    //                 }
-    //             }
-    //         }
+            if (achievement.intervalAlternative) {
+                for await (const alt of achievement.intervalAlternative) {
+                    if (alt.dir === '<' && new_stat < alt.check && interval > alt.interval) {
+                        interval = alt.interval;
+                    } else if (alt.dir === '>' && new_stat > alt.check && interval < alt.interval) {
+                        interval = alt.interval;
+                    }
+                }
+            }
 
-    //         let normalized_old_stat = Math.floor(old_stat / interval);
-    //         let normalized_new_stat = Math.floor(new_stat / interval);
+            let normalized_old_stat = Math.floor(old_stat / interval);
+            let normalized_new_stat = Math.floor(new_stat / interval);
 
-    //         if (normalized_old_stat === normalized_new_stat) continue;
-    //         if (achievement.dir === '>' && normalized_new_stat < normalized_old_stat) continue;
-    //         if (achievement.dir === '<' && normalized_new_stat > normalized_old_stat) continue;
+            if (normalized_old_stat === normalized_new_stat) continue;
+            if (achievement.dir === '>' && normalized_new_stat < normalized_old_stat) continue;
+            if (achievement.dir === '<' && normalized_new_stat > normalized_old_stat) continue;
 
-    //         const reached_milestone = achievement.dir === '>' ? normalized_new_stat : normalized_old_stat;
+            const reached_milestone = achievement.dir === '>' ? normalized_new_stat : normalized_old_stat;
 
-    //         await InspectorUserMilestone.create({
-    //             user_id: user.user_id,
-    //             achievement: achievement.name,
-    //             count: reached_milestone * interval,
-    //             time: new Date()
-    //         });
-    //         console.log(`[MILESTONE] ${user.username} reached ${reached_milestone * interval} (${achievement.name})`)
-    //     }
-    // }
+            await InspectorUserMilestone.create({
+                user_id: user.user_id,
+                achievement: achievement.name,
+                count: reached_milestone * interval,
+                time: new Date()
+            });
+            console.log(`[MILESTONE] ${user.username} reached ${reached_milestone * interval} (${achievement.name})`)
+        }
+    }
 
     console.log(`[CACHER] Calculating global SS ranks ...`);
     //get global ss rank
