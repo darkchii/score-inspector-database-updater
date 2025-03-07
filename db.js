e8be828cb66ac64660442ac9125a01757d082c0a
Mark <engineermarkpro@gmail.com>
2025-03-07 03:00:25 +0100

    added retry

--- db.js
+++ db.js
@@ -19,9 +19,31 @@ const { OsuUserRulesetDataModel } = require("./Models/OsuUserRulesetDataModel");
 require('dotenv').config();
 
 let databases = {
-    inspector: new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASS, { host: process.env.MYSQL_HOST, dialect: 'mariadb', timezone: 'Europe/Amsterdam', logging: false }),
-    osuAlt: new Sequelize(process.env.ALT_DB_DATABASE, process.env.ALT_DB_USER, process.env.ALT_DB_PASSWORD, { host: process.env.ALT_DB_HOST, dialect: 'postgres', logging: false })
+    inspector: new Sequelize(
+        process.env.MYSQL_DB,
+        process.env.MYSQL_USER,
+        process.env.MYSQL_PASS,
+        {
+            host: process.env.MYSQL_HOST,
+            dialect: 'mariadb',
+            timezone: 'Europe/Amsterdam',
+            logging: false,
+            retry: {
+                max: 10, // maximum amount of retries
+                backoffBase: 500, // 1 second
+            }
+        }),
+    osuAlt: new Sequelize(
+        process.env.ALT_DB_DATABASE,
+        process.env.ALT_DB_USER,
+        process.env.ALT_DB_PASSWORD,
+        {
+            host: process.env.ALT_DB_HOST,
+            dialect: 'postgres',
+            logging: false
+        })
 };
+
 module.exports.Databases = databases;
 
 const InspectorOsuUser = OsuUserModel(databases.inspector);
